c96fb7fc981c38f5e3e22e7b53d9aa56cd7c76a4
martinusbe <martinusbe@gmail.com>
2020-04-14 02:05:44 +0200

    op5: make it tipsy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cheeseburger.mk
+    $(LOCAL_DIR)/tipsy_cheeseburger.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_cheeseburger-user \
-    lineage_cheeseburger-userdebug \
-    lineage_cheeseburger-eng
+    tipsy_cheeseburger-user \
+    tipsy_cheeseburger-userdebug \
+    tipsy_cheeseburger-eng
